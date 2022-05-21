cce30f5d3a2e44c2fd936335146f182c15bede4c
josuhee <suheeqwe@gmail.com>
2022-05-21 20:23:47 +0900

    C-10 Tab 정리

--- src/open_file.py
+++ src/open_file.py
@@ -1,24 +1,18 @@
 # 파일 출력 및 corpus, s_corpus 저장
 def open_file(corpus, s_corpus, origin_corpus):
-    '''
-    global corpus
-    global s_corpus
-    global origin_corpus
-    '''
+	f = open("../file/sample-sequence.txt", "r")
+	
+	while True:
+		state = f.readline()
+		if not state:
+			break
+		state = state.strip()
+		origin_corpus.append(state)
+		if state not in corpus:
+			corpus.append(state)
+			state_split = state.split("_")
+			for i in state_split:
+				if i not in s_corpus:
+					s_corpus.append(i)
 
-    f = open("../file/sample-sequence.txt", "r")
-    
-    while True:
-        state = f.readline()
-        if not state:
-            break
-        state = state.strip()
-        origin_corpus.append(state)
-        if state not in corpus:
-            corpus.append(state)
-            state_split = state.split("_")
-            for i in state_split:
-                if i not in s_corpus:
-                    s_corpus.append(i)
-    
-    return corpus, s_corpus, origin_corpus
+	return corpus, s_corpus, origin_corpus
